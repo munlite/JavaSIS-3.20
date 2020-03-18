34216181516f908eee889975e7563ff6c4941716
munlite <wanted635@gmail.com>
2020-03-18 13:35:32 +0700

    homework 3

--- unit4/src/main/java/pro/sisit/adapter/impl/CSVAdapter.java
+++ unit4/src/main/java/pro/sisit/adapter/impl/CSVAdapter.java
@@ -1,32 +1,109 @@
 package pro.sisit.adapter.impl;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import pro.sisit.adapter.IOAdapter;
+import pro.sisit.model.Author;
+import pro.sisit.model.Book;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.io.FileUtils.*;
 
 // 1. TODO: написать реализацию адаптера
 
 public class CSVAdapter<T> implements IOAdapter<T> {
-
     private Class<T> entityType;
-    private BufferedReader reader;
-    private BufferedWriter writer;
+    private Path pathToFIle;
+    private List<String> listOfLinesFromFile;
 
-    public CSVAdapter(Class<T> entityType, BufferedReader reader,
-        BufferedWriter writer) {
+    public CSVAdapter(Class<T> entityType, Path path) {
 
         this.entityType = entityType;
-        this.reader = reader;
-        this.writer = writer;
+        this.pathToFIle = path;
     }
 
     @Override
     public T read(int index) {
-        throw new RuntimeException("Метод read не реализован");
+        listOfLinesFromFile = new ArrayList<>();
+        if (entityType.getName().equals("pro.sisit.model.Author")) {
+            try (LineIterator lineIteratorFile = FileUtils.lineIterator(pathToFIle.toFile())) { //создаем итератор для файла
+                while (lineIteratorFile.hasNext()){
+                    listOfLinesFromFile.add(lineIteratorFile.nextLine()); // записываем все строки в List
+                }
+                if (convertListToObject(index, listOfLinesFromFile) != null) // проверяем полученный объект
+                    return convertListToObject(index, listOfLinesFromFile); // метод, который создает нужный объект с параметрами из листа
+                else throw new NullPointerException();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }else if (entityType.getName().equals("pro.sisit.model.Book")){
+            try (LineIterator lineIteratorFile = lineIterator(pathToFIle.toFile());) {
+                while (lineIteratorFile.hasNext()){
+                    listOfLinesFromFile.add(lineIteratorFile.nextLine());
+                }
+                if (convertListToObject(index, listOfLinesFromFile) != null)
+                    return convertListToObject(index, listOfLinesFromFile);
+                else throw new NullPointerException();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
 
     @Override
     public int append(T entity) {
-        throw new RuntimeException("Метод append не реализован");
+        int indexLineInFile =1; // индекс последней строки при нахождении объекта
+
+        if (entity instanceof Author) {
+            boolean checkingSuccessfulSearch = false; //флаг, используемый для определения нахождения нужного поля
+            try (LineIterator lineIteratorFile = lineIterator(pathToFIle.toFile());) {
+                while (lineIteratorFile.hasNext()) {
+                    if (((Author) entity).getName().equals(lineIteratorFile.nextLine())) {
+                        checkingSuccessfulSearch = true;
+                        indexLineInFile++;
+                        break;
+                    } else indexLineInFile++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return checkingSuccessfullSearchInFile(checkingSuccessfulSearch, indexLineInFile,2); //метод для перевода индекса последней строи в индек объекта
+        } else if (entity instanceof Book) {
+            boolean checkingSuccessfulSearch = false;
+            try (LineIterator lineIteratorFile = lineIterator(pathToFIle.toFile());) {
+                while (lineIteratorFile.hasNext()) {
+                    if (((Book) entity).getIsbn().equals(lineIteratorFile.nextLine())) {
+                        checkingSuccessfulSearch = true;
+
+                        break;
+                    } else indexLineInFile++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return checkingSuccessfullSearchInFile(checkingSuccessfulSearch, indexLineInFile,4);
+        }
+        return indexLineInFile;
     }
+    private int checkingSuccessfullSearchInFile(boolean checkingSuccessfulSearch, int indexLineInFile, int numberOfLines){
+        if (checkingSuccessfulSearch)
+            return indexLineInFile/numberOfLines;
+        else return -1;
+    }
+    private T convertListToObject(int rowIndex, List<String> list){
+        if (entityType.getName().equals("pro.sisit.model.Author")){
+            return (T) new Author(list.get(rowIndex*2-2), list.get(rowIndex*2-1));
+        }
+        else if (entityType.getName().equals("pro.sisit.model.Book")){
+            return (T) new Book(list.get(rowIndex*4-4), list.get(rowIndex*4-3), list.get(rowIndex*4-2), list.get(rowIndex*4-1));
+        }else return null;
+    }
+
 }
--- unit4/src/test/java/pro/sisit/adapter/CSVAdapterTest.java
+++ unit4/src/test/java/pro/sisit/adapter/CSVAdapterTest.java
@@ -1,35 +1,65 @@
 package pro.sisit.adapter;
 
-import static org.junit.Assert.assertEquals;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Test;
+import pro.sisit.adapter.impl.CSVAdapter;
+import pro.sisit.model.Author;
+import pro.sisit.model.Book;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import pro.sisit.adapter.impl.CSVAdapter;
-import pro.sisit.model.Book;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 // TODO: 2. Описать тестовые кейсы
 
 public class CSVAdapterTest {
 
-    @Before
+    @Test
     public void createFile() {
+        Path authorFilePath = Paths.get("test-author-file.csv");
+        File file =new File(authorFilePath.toUri());
+        file.exists();
+        Author author = new Author("Глуховский", "Москва");
+        CSVAdapter<Author> authorCsvAdapter =
+                new CSVAdapter(Author.class, authorFilePath);
         // TODO: создать и заполнить csv-файл для сущности Author
-        // TODO: создать и заполнить csv-файл для сущности Book
+        try {
+            FileUtils.writeStringToFile(authorFilePath.toFile(), String.format(System.lineSeparator()+"%s\n%s", author.getName(), author.getBirthPlace()), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals("test-author-file.csv", Paths.get("test-author-file.csv"));
 
-        // * По желанию можете придумать и свои сущности
+        //authorCsvAdapter.append(author);
+        // TODO: создать и заполнить csv-файл для сущности Book
+//        Path bookFilePath = Paths.get("test-book-file.csv");
+//        Book newBook = new Book(
+//                "Чертоги разума. Убей в себе идиота!",
+//                "Андрей Курпатов",
+//                "Психология",
+//                "978-5-906902-91-7");
+//        try {
+//            FileUtils.writeStringToFile(bookFilePath.toFile(), String.format(System.lineSeparator()+"%s\n%s\n%s\n%s", newBook.getName(),
+//                    newBook.getAuthor(), newBook.getGenre(), newBook.getIsbn()), true);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        assertEquals("test-book-file.csv", Paths.get("test-book-file.csv"));
+//        // * По желанию можете придумать и свои сущности
     }
 
     @After
     public void deleteFile() {
         // TODO: удалить файлы после тестирования
+        Path authorFilePath = Paths.get("test-author-file.csv");
+        FileUtils.deleteQuietly(authorFilePath.toFile());
+        Path bookFilePath = Paths.get("test-book-file.csv");
+        FileUtils.deleteQuietly(bookFilePath.toFile());
     }
 
     @Test
@@ -37,56 +67,54 @@ public class CSVAdapterTest {
 
         Path bookFilePath = Paths.get("test-book-file.csv");
 
-        BufferedReader bookReader = new BufferedReader(
-            new FileReader(bookFilePath.toFile()));
-
-        BufferedWriter bookWriter = new BufferedWriter(
-            new FileWriter(bookFilePath.toFile(), true));
-
         CSVAdapter<Book> bookCsvAdapter =
-            new CSVAdapter(Book.class, bookReader, bookWriter);
+                new CSVAdapter(Book.class, bookFilePath);
 
-        Book book1 = bookCsvAdapter.read(1);
+        Book book1 = bookCsvAdapter.read(3);
         assertEquals("Глуховский", book1.getAuthor());
         assertEquals("Будущее", book1.getName());
         assertEquals("978-5-17-118366-0", book1.getIsbn());
         assertEquals("Научная фантастика", book1.getGenre());
 
-        Book expectedBook0 = new Book(
-            "Убик",
-            "Филип Дик",
-            "Научная фантастика",
-            "978-5-699-97309-5");
-        Book actualBook0 = bookCsvAdapter.read(0);
-        assertEquals(expectedBook0, actualBook0);
-
         // TODO: написать тесты для проверки сущности автора
+
+        Path authorFilePath = Paths.get("test-author-file.csv");
+
+        CSVAdapter<Author> authorCsvAdapter =
+                new CSVAdapter(Author.class, authorFilePath);
+        Author author = authorCsvAdapter.read(3);
+        assertEquals("Москва", author.getBirthPlace());
+        assertEquals("Пушкин", author.getName());
     }
 
     @Test
     public void testAppend() throws IOException {
 
-        Path bookFilePath = Paths.get("test-book-file.csv");
+        Path authorFilePath = Paths.get("test-author-file.csv");
+        Author newAuthor = new Author("Тютчев","Овстуг");
+        CSVAdapter<Author> authorCSVAdapter =
+                new CSVAdapter(Author.class, authorFilePath);
 
-        BufferedReader bookReader = new BufferedReader(
-            new FileReader(bookFilePath.toFile()));
+        int authorIndex = authorCSVAdapter.append(newAuthor);
+        Author authorAtIndex = authorCSVAdapter.read(authorIndex);
+        System.out.println(authorIndex);
+        System.out.println(authorAtIndex.getName());
+        assertEquals(newAuthor, authorAtIndex);
 
-        BufferedWriter bookWriter = new BufferedWriter(
-            new FileWriter(bookFilePath.toFile(), true));
-
-        CSVAdapter<Book> bookCsvAdapter =
-            new CSVAdapter(Book.class, bookReader, bookWriter);
-
-        Book newBook = new Book(
-            "Чертоги разума. Убей в себе идиота!",
-            "Андрей Курпатов",
-            "Психология",
-            "978-5-906902-91-7");
-
-        int bookIndex = bookCsvAdapter.append(newBook);
-        Book bookAtIndex = bookCsvAdapter.read(bookIndex);
-        assertEquals(newBook, bookAtIndex);
-
-        // TODO: написать тесты для проверки сущности автора
+//        Path bookFilePath = Paths.get("test-book-file.csv");
+//        CSVAdapter<Book> bookCsvAdapter =
+//                new CSVAdapter(Book.class, bookFilePath);
+//
+//        Book newBook = new Book(
+//                "1",
+//                "2",
+//                "3",
+//                "4");
+//
+//        int bookIndex = bookCsvAdapter.append(newBook);
+//        Book bookAtIndex = bookCsvAdapter.read(bookIndex);
+//        assertEquals(newBook, bookAtIndex);
+//
+//        // TODO: написать тесты для проверки сущности автора
     }
 }
